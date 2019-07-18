6b14a7a7621cb6c1a94b2a93afa7d6f14a851e17
sernalab <carles.serna@gmail.com>
2019-07-18 10:42:43 +0200

    Nav component created

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="navbar col-2">
+        <router-link to="/">Search</router-link> |
+        <router-link to="/card-game">Card Game</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style>
+
+</style>
